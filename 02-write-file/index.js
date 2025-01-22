4d5b7001f417f56b98e3f6a33deea9818a899bb1
Tsubasa <katakiritsubasa@gmail.com>
2025-01-22 20:25:17 +0300

    fix: fix a bug when the exit message is not display

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -5,11 +5,12 @@ const path = require('path');
 const file = fs.createWriteStream(path.join(__dirname, 'text.txt'));
 
 process.stdout.write(
-  'Welcome to 02-write-file task! Type some text and press ENTER to write it into text.txt. \nType exit or press CTRL + C to exit\n',
+  'Welcome to 02-write-file task! Type some text and press ENTER to write it into text.txt. \nType exit and press ENTER or press CTRL + C to exit\n',
 );
 
 const rl = readline.createInterface({
   input: process.stdin,
+  output: process.stdout,
 });
 
 rl.on('line', (data) => {
